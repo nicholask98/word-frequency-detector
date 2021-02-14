9828920c7885f86bb496ba2629697825169b963b
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-02-14 15:46:12 -0800

    Fixed last word problem
    Last word now is counted the same as the other words.

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 user_doc = input('Paste your document here: \n')
 
-words_used = {'test': 1}
+words_used = {}
 
 # Create new string for each new word based on spaces
 
-'''for character in range(len(user_doc)):
-    current_word = ''
-    if user_doc[character] != ' ':
-        current_word = current_word + user_doc[character]
-        print(current_word)
-    elif user_doc[character] == ' ':
-        print('this is a space')
-'''
 current_word = ''
 for character in range(len(user_doc)):
-    # FIXME: check if existing word already and add one to total for word
-    
-    # For new words
+    # Compiles all characters before the next space into one string: current_word
     if user_doc[character] != ' ':
         current_word += user_doc[character]
 
         # FIXME: check last word in document if it doesn't have a space
-    elif (user_doc[character] == ' '): # or character == len(user_doc):
+    elif (user_doc[character] == ' '):
+
+# ----------------------------
+# (1)Make this if/else a function
         if current_word in words_used:
             words_used[current_word] += 1
-        else:
+        else: # For new words
             words_used[current_word] = 1
         current_word = ''
+# ----------------------------
+# ----------------------------
+# (1)Make this if/else a function
+if current_word in words_used:
+    words_used[current_word] += 1
+else: # For new words
+    words_used[current_word] = 1
+    current_word = ''
+# ----------------------------
 
 print(words_used)
 
